f08fb888befbe16cf9c7bc1ff5a56ded609ec86e
Nayan P Ingale <97461960+NayanIngale1@users.noreply.github.com>
2022-04-03 12:06:51 +0530

    Tic Tac Toe Game

---
+++ JS/script.js
@@ -0,0 +1,46 @@
+import Game from "./Game.js";
+import GameView from "./GameView.js";
+
+let game = new Game();
+let gameView = new GameView();
+
+let tiles = document.querySelectorAll(".board-tile");
+
+document.querySelector(".restart").addEventListener("click", () => {
+  startNewGame();
+});
+
+tiles.forEach((tile) => {
+  tile.addEventListener("click", () => {
+    onTileClick(tile.dataset.index);
+    // console.log('tile.dataset.index:', tile.dataset.index)
+  });
+});
+
+function onTileClick(i) {
+  //make move
+  game.makeMove(i)
+  // update board
+  gameView.updateBoard(game);
+}
+
+function startNewGame() {
+  game = new Game();
+  gameView.updateBoard(game);
+}
+
+  gameView.updateBoard(game);
+
+
+let DarkBtn = document.getElementById('btn');
+ if (DarkBtn.checked == true) {
+   document.body.classList.add("dark");
+ }
+ DarkBtn.addEventListener("change", function () {
+  if (this.checked == true) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+ });
+  
